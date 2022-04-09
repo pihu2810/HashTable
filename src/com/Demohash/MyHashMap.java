a1ca65a38c8109187577861bd66c29ae86863d88
pihu2810 <vishaka.kadam01@gmail.com>
2022-04-09 22:12:51 +0530

    add HashTableUC3

--- src/com/Demohash/MyHashMap.java
+++ src/com/Demohash/MyHashMap.java
@@ -1,33 +1,59 @@
 package com.Demohash;
 
+import java.util.ArrayList;
 
 public class MyHashMap<K,V> 
 {
-	MyLinkedList<K> myLinkedList;
+	
+		private final int numOfBuckets;
+		ArrayList<MyLinkedList<K>> myBucketArray;
 
-	public MyHashMap() {
-		this.myLinkedList = new MyLinkedList<>();
-	}
-
-	public V get(K key) {
-		MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) this.myLinkedList.search(key);
-		return (myMapNode == null) ? null : myMapNode.getValue();
-	}
-
-	public void add(K key, V value) {
-		MyMapNode<K,V> myMapNode = (MyMapNode<K, V>) this.myLinkedList.search(key);
-		if(myMapNode == null) {
-			myMapNode = new MyMapNode<K,V>(key, value);
-			this.myLinkedList.append(myMapNode);
+		public MyHashMap() {
+			this.numOfBuckets = 10;
+			this.myBucketArray = new ArrayList<>(numOfBuckets);	
+			for (int i = 0; i < numOfBuckets; i++ )
+				this.myBucketArray.add(null);
 		}
-		else {
-			myMapNode.setValue(value);
+		private int getBucketIndex(K key) {
+			int hashCode = Math.abs(key.hashCode()); 
+			int index = hashCode % numOfBuckets;
+			return index;
 		}
-	}
 
-	@Override
-	public String toString() {
-		return "MyHashMap [myLinkedList = " + myLinkedList + "]";
-	}
-
+		public V get(K key) {
+			int index = this.getBucketIndex(key);
+			MyLinkedList<K> myList = this.myBucketArray.get(index);
+			if (myList == null)
+				return null;
+			MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) myList.search(key);
+			return (myMapNode == null) ? null : myMapNode.getValue();
+		}
+		
+		public void add(K key, V value) {
+			int index = this.getBucketIndex(key);
+			MyLinkedList<K> myLinkedList = this.myBucketArray.get(index);
+			if (myLinkedList  == null) {
+				myLinkedList  = new MyLinkedList<>();
+				this.myBucketArray.set(index, myLinkedList );
+			}
+			MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) myLinkedList .search(key);
+			if (myMapNode == null) {
+				myMapNode = new MyMapNode<>(key, value);
+				myLinkedList .append(myMapNode);
+			} else {
+				myMapNode.setValue(value);
+			}
+		}
+		
+		public void removeKey(K key) {
+			int index = this.getBucketIndex(key);
+			MyLinkedList<K> myLinkedList = this.myBucketArray.get(index);
+			MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) myLinkedList.search(key);
+			myLinkedList.delete(key);
+			myBucketArray.remove(index);
+		}
+		
+    public String toString() {
+			return "MyHashMap [numOfBuckets=" + numOfBuckets + ", myBucketArray=" + myBucketArray + "]";
+		}
 }
